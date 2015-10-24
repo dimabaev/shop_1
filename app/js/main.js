24e4370126e0757317c8e1ee028d1948dcf6c98f
dimabaev <welheor666@gmail.com>
2015-10-25 00:08:21 +0300

    22323

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+//slider 
+    $( "#slider" ).slider({
+      range: true,
+      animate: "normal",
+      min: 0,
+      max: 12000,
+      values: [ 3000, 6000 ],
+        stop: function(event, ui) {
+          jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+          jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+    },
+        slide: function(event, ui){
+          jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+          jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+    }
+  });
